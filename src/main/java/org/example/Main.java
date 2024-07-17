d90b84bce7762dc8a47cda3b1e341369efe3d27d
Siroj-Ruzmatov <ruzmatovsiroj093@gmail.com>
2024-07-17 12:48:53 +0500

    menu and getMenuNumber methods have written in Mani class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        }
+       while (true) {
+           menu();
+           int n = getMenuNumber();
+           switch (n){
+               case 1:
+           }
+       }
+
     }
+
+    public static void menu(){
+        System.out.println("*** Menu ***\n" + "1.Contact add\n" + "2.Contact List\n" + "3.Searching Contacts\n"
+                + "4.Delete Contact(by phone number)\n" + "0.Exit");
+    }
+
+    public static int getMenuNumber(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose a mune number: ");
+        return scanner.nextInt();
+    }
+}
