78bb17018cfa78aff53c9e14419bd84439a991d3
Siroj-Ruzmatov <ruzmatovsiroj093@gmail.com>
2024-08-06 11:32:34 +0500

    Contact class added

---
+++ src/main/java/org/example/Contact.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Contact {
+    private String name;
+    private String surname;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact [name=" + name + ", surname=" + surname + ", phone=" + phone;
+    }
+}
